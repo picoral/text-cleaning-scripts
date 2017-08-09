e19d02f3ff7534d535700e24ac32a8dadf9d5d76
Adriana Picoral <adrianaps@email.arizona.edu>
2017-08-08 18:43:35 -0700

    Python Scripts - these run with Babun on Windows

---
+++ cyrillictounicode.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+import re
+import glob
+import codecs
+
+
+input_filename = 'unknown8.txt'
+input_file = open(input_filename, 'r')
+
+for line in input_file:
+    filename = re.sub(r'[\n|\r]',r'',line)
+    f = codecs.open(filename, 'r', 'cp1251')
+    
+    # get the file name
+    file_name = re.sub(r'\.txt',r'',f.name)
+    file_name = re.sub(r'\.TXT',r'',file_name)
+
+    # get file path
+    matches = re.findall('[\w\s]+\/', file_name)
+    path = ''
+    for m in matches:
+        path = path + m
+
+    #path = path + "normed/"
+
+    file_name = re.sub(r'[\w\s]+\/',r'',file_name)
+
+    # output file name
+    output_file_name = path + file_name + ".txt"
+    print(output_file_name)
+
+
+    u = f.read()   # now the contents have been transformed to a Unicode string
+    out = codecs.open(output_file_name, 'w', 'utf-8')
+    out.write(u)
+    out.close()
+    
